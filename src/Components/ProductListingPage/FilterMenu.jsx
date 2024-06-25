0b8e16c38623b5238caf810df1ced455b16bdd54
keerthi <keerthidhiva07@gmail.com>
2024-06-25 00:18:28 -0400

    filtermenu

--- src/Components/ProductListingPage/FilterMenu.jsx
+++ src/Components/ProductListingPage/FilterMenu.jsx
@@ -3,13 +3,35 @@ import React,{useState} from 'react';
 
 const FilterMenu = ({emitFn}) => {
 
-      const [freeshipping, setfreeshipping] = useState(null);
-      const [availabilityStock, setAvailabilityStock] = useState(null);
-      const [pricing, setPricing] = useState(null);
-      const [bestSelling, setBestSelling] = useState(null);
+          const [filterObj, setFilterObj] = useState({shipping:false, availability:false, price: false, bestSelling: false});
 
-
-      
+      const handleInpChg = (event) => {
+        if(event.target.name === "free-shipping"){
+          filterObj.shipping = event.target.checked;
+          setFilterObj({...filterObj});
+         
+        }
+        else if(event.target.name === "availability-out-of-stock"){
+          filterObj.availability = event.target.checked;
+          setFilterObj({...filterObj});
+        }
+        else if(event.target.name === "filter-price"){
+          if(event.target.value === "min"){
+            filterObj.price = event.target.value;
+            setFilterObj({...filterObj});
+          }
+          else{
+            filterObj.price = event.target.value;
+            setFilterObj({...filterObj});
+          }
+             
+        }
+        else if(event.target.name === "filter-best-selling"){
+          filterObj.bestSelling = event.target.checked;
+          setFilterObj({...filterObj});
+        }
+        emitFn({...filterObj});
+      }
 
   
   return (
@@ -19,34 +41,34 @@ const FilterMenu = ({emitFn}) => {
             <div className='filter-box'>
                 <div className='eligible-free-shipping-'>
                 <h4>Eligible for Free Shipping</h4>
-                <input type="checkbox" id="free-shipping" name="free-shipping"  onChange = {(e)=>{
-                  emitFn({shipping: e.target.checked})
+                <input type="checkbox" id="free-shipping" name="free-shipping"  onChange = {(event)=>{
+                  handleInpChg(event);
                 }}    />
                 <label for="free-shipping"> FREE Shipping</label>
             </div>
             <div className='filter-availability-stock'>
                 <h4>Availability</h4>
-                <input type="checkbox" id="availability-out-of-stock" name="availability-out-of-stock" value="availability-out-of-stock" onChange={(e)=>{setAvailabilityStock(e.target.checked)
-                  emitFn({outOfStock: true})
-                }}/>
+                <input type="checkbox" id="availability-out-of-stock" name="availability-out-of-stock" value="availability-out-of-stock" onChange = {(event)=>{
+                  handleInpChg(event);
+                }} />
                 <label for="availability-out-of-stock">Include Out of stock</label>
             </div>
             <div className='filter-pricing'>
                 <h4>Sort by</h4>
-                <input type="radio" id="filter-pricing-min" name="filter-price" value="min" onChange = {(e)=>{setPricing(e.target.value)
-                  emitFn({lowPrice: true})
-                }}/>
+                <input type="radio" id="filter-pricing-min" name="filter-price" value="min" onChange = {(event)=>{
+                  handleInpChg(event);
+                }} />
                 <label for="filter-pricing-min">Price: Low to High</label>
-                <input type="radio" id="filter-pricing-max" name="filter-price" value="max" onChange = {(e)=>{setPricing(e.target.value)
-                  emitFn({highPrice: true})
-                }}/>
+                <input type="radio" id="filter-pricing-max" name="filter-price" value="max" onChange = {(event)=>{
+                  handleInpChg(event);
+                }} />
                 <label for="filter-pricing-max">Price: High to Low</label>
             </div>
             <div className='filter-best-selling'>
                 <h4>Best Selling</h4>
-                <input type="checkbox" id="best-selling" name="filter-pricing-max" value="filter-pricing-max" onChange={(e)=>{setBestSelling(e.target.checked)
-                  emitFn({bestSelling: true})
-                }}/>
+                <input type="checkbox" id="best-selling" name="filter-best-selling" value="filter-pricing-max" onChange = {(event)=>{
+                  handleInpChg(event);
+                }} />
                 <label for="filter-pricing-max">Sort by best selling</label>
             </div>
             </div>
--- src/Components/ProductListingPage/ProductListingArea.jsx
+++ src/Components/ProductListingPage/ProductListingArea.jsx
@@ -6,26 +6,12 @@ import {headPhonesArr} from '../../JsonData/HeadPhones';
 const ProductListingArea = ({filterMenuData}) => {
 
   const[data, setData] = useState(headPhonesArr);
-  const[filterData, setFilterData] = useState(headPhonesArr);
-
   
 
-  // useEffect (()=>{
-  //   if(filterMenuData.hasOwnProperty("shipping")){
-  //     setData(headPhonesArr);
-  //     const eligibleFreeShipping = () =>{
-  //         let temp = [...data];
-  //         console.log(temp);
-  //         temp = temp.filter((item, index)=>{
-  //           return item?.freeShipping === filterMenuData?.shipping
-  //         })
-  //         setData(temp);
-          
-  //     }
-  //     eligibleFreeShipping();
-  //   }
-  // },[filterMenuData?.shipping])
+  console.log(filterMenuData);
 
+  useEffect(()=>{})
+  
   const dateFormatFn = (dates) =>{
       const date = new Date();
       date.setDate(date.getDate() + dates);
@@ -52,91 +38,13 @@ const ProductListingArea = ({filterMenuData}) => {
         <div className='product-listing-area-container'>
             <div className='product-listing-row row m-0'>
 
-              {data.map((item, index)=>{
-
-                if(filterMenuData.hasOwnProperty("shipping")){
-
-                  if(filterMenuData?.shipping===item.freeShipping){
-
-                    return(
-                      <div className='product-listing-col col-lg-4 my-3'>
-                      <div className='product-listing-box'>
-                          <div className='product-listing-item'>
-                            <div className='product-listing-image text-center'>
-                              <img src={item.thumbnailImg} /> 
-                            </div>
-                            <div className='product-listing-details'>
-                            <p className='product-listing-name'>{item.name}</p>
-                            <span className='product-listing-rating'>
-                                <i class="bi bi-star-fill"></i>
-                                <i class="bi bi-star-fill"></i>
-                                <i class="bi bi-star-fill"></i>
-                                <i class="bi bi-star-fill"></i>
-                                <i class="bi bi-star-half"></i>
-                                <span>{item.numberOfRatings}</span>
-                            </span>
-                            <p>{item.noOfOrdersPastMonth} bought in past month</p>
-                            <p>{item.buyingPrice}</p>
-                            {item.mrpPrice ?(<p>{item.mrpPrice}</p>) : null}
-                            {item.freeShipping ? (<p>Free delivery {dateFormatFn(item.freeDeliveryDate)}</p>) : (<p>Free delivery {dateFormatFn(item.freeDeliveryDate)} on $35 of items shipped by Amazon </p>)}
-                            <p>Or fastest delivery {dateFormatFn(item.fastDeliveryDate)}</p>
-                            <button>Add to cart</button>
-                            </div>
-                            
-                          </div>
-                      </div>
-                     </div>
-                    )
-  
-  
-                  }
-                }
-
-                else{
-
-                  return(
-                    <div className='product-listing-col col-lg-4 my-3'>
-                    <div className='product-listing-box'>
-                        <div className='product-listing-item'>
-                          <div className='product-listing-image text-center'>
-                            <img src={item.thumbnailImg} /> 
-                          </div>
-                          <div className='product-listing-details'>
-                          <p className='product-listing-name'>{item.name}</p>
-                          <span className='product-listing-rating'>
-                              <i class="bi bi-star-fill"></i>
-                              <i class="bi bi-star-fill"></i>
-                              <i class="bi bi-star-fill"></i>
-                              <i class="bi bi-star-fill"></i>
-                              <i class="bi bi-star-half"></i>
-                              <span>{item.numberOfRatings}</span>
-                          </span>
-                          <p>{item.noOfOrdersPastMonth} bought in past month</p>
-                          <p>{item.buyingPrice}</p>
-                          {item.mrpPrice ?(<p>{item.mrpPrice}</p>) : null}
-                          {item.freeShipping ? (<p>Free delivery {dateFormatFn(item.freeDeliveryDate)}</p>) : (<p>Free delivery {dateFormatFn(item.freeDeliveryDate)} on $35 of items shipped by Amazon </p>)}
-                          <p>Or fastest delivery {dateFormatFn(item.fastDeliveryDate)}</p>
-                          <button>Add to cart</button>
-                          </div>
-                          
-                        </div>
-                    </div>
-                   </div>
-                  )
-
-                }
-
-                
-              })}
+              
 
 
-            {/* {data.map((item, index)=> 
-
-
-
+            {data.map((item, index)=> 
             {
               return(
-                <div className='product-listing-col col-lg-4 my-3'>
+                <div key={item.id} className='product-listing-col col-lg-4 my-3'>
                 <div className='product-listing-box'>
                     <div className='product-listing-item'>
                       <div className='product-listing-image text-center'>
@@ -165,7 +73,7 @@ const ProductListingArea = ({filterMenuData}) => {
                </div>
               )
             }              
-            )} */}
+            )}
                 
             </div>
         </div>
